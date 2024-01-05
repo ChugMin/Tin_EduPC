b17d4bafd4ec31974e5572727e3699cd090229e2
ChugMin <113907009+ChugMin@users.noreply.github.com>
2024-01-05 19:57:45 +0700

    Add files via upload

--- bai10.py
+++ bai10.py
@@ -5,8 +5,8 @@
 num = int(input("Nhập vào 1 số nguyên bất kỳ: "))
 
 gio = num // 3600
-tmp = num  % 3600
-phut = num % 360
-giay = tmp // 60
+tmp = num % 3600
+phut = tmp // 60
+giay = tmp % 60
 
 print(gio, "giờ", phut, "phút", giay, "giây")

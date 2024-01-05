cd046db129e8b1874b54702da8509322da66b105
ChugMin <113907009+ChugMin@users.noreply.github.com>
2024-01-05 20:00:12 +0700

    Add files via upload

--- bai9.py
+++ bai9.py
@@ -1,4 +1,4 @@
-num = int(input("Nhập vào 1 số có 3 chữ số: "))
+num = int(input("Nhập vào 1 số có 4 chữ số: "))
 
 sum = num % 10
 num //= 10
@@ -6,6 +6,9 @@ sum += num  % 10
 num //= 10
 sum += num % 10
 num//= 10
-sum += num
+sum += num % 10
+num //= 10
+sum += num % 10
 
 print("Tong cua cac chu so cua so do la:", sum)
+
